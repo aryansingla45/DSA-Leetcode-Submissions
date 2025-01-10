2b31f9ac4955e191cb6aa4c2d22ac71a4b853eaa
Aryan Singla <62651987+aryansingla45@users.noreply.github.com>
2025-01-10 22:37:10 +0530

    10-jan-dailies

---
+++ Word-Subsets-916.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxFreq = new int[26];
+        for (String word : words2) {
+            int[] tempFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                tempFreq[c - 'a']++;
+                maxFreq[c - 'a'] = Math.max(maxFreq[c - 'a'], tempFreq[c - 'a']);
+            }
+        }
+
+        
+        List<String> result = new ArrayList<>();
+
+
+        for (String word : words1) {
+            int[] wordFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                wordFreq[c - 'a']++;
+            }
+
+            boolean f = true;
+            for (int i = 0; i < 26; i++) {
+                if (wordFreq[i] < maxFreq[i]) {
+                    f = false;
+                    break;
+                }
+            }
+
+            if (f) {
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+}
