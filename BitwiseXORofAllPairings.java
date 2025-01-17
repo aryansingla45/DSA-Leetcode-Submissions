e062e3ff1e16c6d7a73c163186c62427ff55ce47
Aryan Singla <62651987+aryansingla45@users.noreply.github.com>
2025-01-17 23:54:42 +0530

    Create BitwiseXORofAllPairings.java

---
+++ BitwiseXORofAllPairings.java
@@ -0,0 +1,24 @@
+class BitwiseXORofAllPairings {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+
+        int xor = 0;
+        
+        if(l2 % 2 != 0){
+            for(int num : nums1){
+                xor ^= num;
+            }
+        }
+
+        if(l1 % 2 != 0){
+            for(int num : nums2){
+                xor ^= num;
+            }
+        }
+
+        return xor;
+        
+    }
+}
